d6057cd3b7a815ffb4df370978fa727dd103d0d1
ocrossi <octave.rossi@gmail.com>
2025-10-10 07:07:33 +0200

    step 7

--- includes/woody.h
+++ includes/woody.h
@@ -11,9 +11,9 @@ typedef struct s_woodyData {
     char *file_bytes;
     char *output_bytes;
     void *e_entry;
-    void *pt_note_pos;
 
     size_t file_size;
+    size_t offset_ptnote; // how many bytes starting at the beginning of the file
  
     Elf64_Phdr pt_note;
     Elf64_Ehdr elf_hdr;
